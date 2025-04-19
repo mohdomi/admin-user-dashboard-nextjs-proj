73f010c178a94f1a4e2af09499424e2339f6417b
mohdomi <omairm.s.chouhan@gmail.com>
2025-04-19 12:25:06 +0000

    added the accept,block,delete options and fixed some bugs

--- app/api/accept-users/route.ts
+++ app/api/accept-users/route.ts
@@ -1,27 +1,33 @@
+import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prismaClient";
-import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { collegeRollNoArray, accepted, status } = body;
+
+    console.log(body);
+
+    if (!collegeRollNoArray || !collegeRollNoArray.length) {
+      return new NextResponse("Roll numbers are required", { status: 400 });
+    }
 
 
-
-export async function POST(req : NextRequest){
-
-    const {college_roll_no , accepted } = await req.json();
-
-    const response = await prisma.user.update({
-        where : {
-            college_roll_no
-        } ,
-        data : {
-            accepted : accepted
+    const users = await prisma.user.updateMany({
+      where: {
+        college_roll_no: {
+            in : collegeRollNoArray
         }
-    })
+      },
+      data: {
+        accepted : accepted,
+        status : status,
+      },
+    });
 
-    return NextResponse.json({
-        response
-    })
-
-
-
-
-
+    return NextResponse.json(users);
+  } catch (error) {
+    console.log("[ACCEPT_USER]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
